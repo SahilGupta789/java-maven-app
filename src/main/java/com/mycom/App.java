940256f83346d5769f0ffd8259973f7f17dc5dd9
SahilGupta789 <112715879+SahilGupta789@users.noreply.github.com>
2025-08-31 18:26:38 +0530

    Create App.java

---
+++ src/main/java/com/mycom/App.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
